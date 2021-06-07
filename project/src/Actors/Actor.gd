ec649bd7c2b8bf51cb41102a1dcee9bda1a952ba
Kinga <k.zawarty@intermarum.pl>
2021-06-07 17:56:46 +0200

    limiting player speed by gravity factor

--- project/src/Actors/Actor.gd
+++ project/src/Actors/Actor.gd
@@ -1,6 +1,12 @@
 extends KinematicBody2D
 class_name Actor
 
-func _physics_process(delta: float) -> void:
-	var velocity: = Vector2(300,0)
-	move_and_slide(velocity)
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 500.0
+var velocity: = Vector2.ZERO
+
+#function similar to unity Update
+func _physics_process(delta: float) -> void:	
+	velocity.y += gravity * delta #delta = time elapsed til last frame => move constantly
+	#velocity.y = max(velocity.y, speed.y)
+	velocity = move_and_slide(velocity)
