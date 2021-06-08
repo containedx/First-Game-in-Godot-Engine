813ffdba9a229f3ca74e057d1851c43a9cc1361b
Kinga <k.zawarty@intermarum.pl>
2021-06-08 12:50:00 +0200

    jumping

--- project/src/Actors/Actor.gd
+++ project/src/Actors/Actor.gd
@@ -1,12 +1,9 @@
 extends KinematicBody2D
 class_name Actor
 
+const FLOOR_NORMAL = Vector2.UP
+
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 500.0
 var velocity: = Vector2.ZERO
 
-#function similar to unity Update
-func _physics_process(delta: float) -> void:	
-	velocity.y += gravity * delta #delta = time elapsed til last frame => move constantly
-	#velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
--- project/src/Actors/Player.gd
+++ project/src/Actors/Player.gd
@@ -1,11 +1,27 @@
 extends Actor
 
+
 func _physics_process(delta):
-	var direction = Vector2(
+	var direction = calculate_direction()
+	velocity = calculate_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	
+func calculate_direction() -> Vector2: 
+		return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		0.0
+		# -1 => jumping,  1 => falling down
+		-1.0 if Input.get_action_strength("jump") and is_on_floor() else 1.0
 	);
-	velocity = speed * direction
+	
+func calculate_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
+	var new_velocity = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time() #delta = time elapsed til last frame => move constantly
+	
+	if direction.y == -1.0: # jumping
+		new_velocity.y = speed.y * direction.y
+		
+	return new_velocity
 	
 
 
