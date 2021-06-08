5407cdb7c7bc635d0c8f11ad448762ac8471bc2f
Kinga <k.zawarty@intermarum.pl>
2021-06-08 19:50:50 +0200

    enemy movement

--- project/src/Actors/Enemy.gd
+++ project/src/Actors/Enemy.gd
@@ -1,2 +1,10 @@
-extends "res://src/Actors/Actor.gd"
+extends Actor
 
+#analogic to unity start()
+func _ready():
+	_velocity.x = -speed.x #move towards left at start
+
+func _physics_process(delta):
+	if is_on_wall():
+		_velocity.x *= -1.0 #turn back when hit the wall
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
