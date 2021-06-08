d61520114f9ba7f839d00d90ac90e2d8846909eb
Kinga <k.zawarty@intermarum.pl>
2021-06-08 12:55:46 +0200

    jump interrupting

--- project/src/Actors/Player.gd
+++ project/src/Actors/Player.gd
@@ -2,8 +2,9 @@ extends Actor
 
 
 func _physics_process(delta):
+	var is_jumping = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var direction = calculate_direction()
-	velocity = calculate_velocity(velocity, direction, speed)
+	velocity = calculate_velocity(velocity, direction, speed, is_jumping)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 	
 func calculate_direction() -> Vector2: 
@@ -13,14 +14,15 @@ func calculate_direction() -> Vector2:
 		-1.0 if Input.get_action_strength("jump") and is_on_floor() else 1.0
 	);
 	
-func calculate_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
+func calculate_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, is_jumping: bool) -> Vector2:
 	var new_velocity = linear_velocity
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time() #delta = time elapsed til last frame => move constantly
 	
 	if direction.y == -1.0: # jumping
 		new_velocity.y = speed.y * direction.y
-		
+	if is_jumping:
+		new_velocity.y = 0.0
 	return new_velocity
 	
 
