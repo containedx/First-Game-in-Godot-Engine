afae1497fa24f6eb20143d9c559d289276090f0e
Kinga <k.zawarty@intermarum.pl>
2021-06-08 19:53:25 +0200

    enemy movement improvements

--- project/src/Actors/Enemy.gd
+++ project/src/Actors/Enemy.gd
@@ -5,6 +5,7 @@ func _ready():
 	_velocity.x = -speed.x #move towards left at start
 
 func _physics_process(delta):
+	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0 #turn back when hit the wall
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
