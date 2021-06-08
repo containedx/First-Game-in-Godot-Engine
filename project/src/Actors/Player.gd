c749f5e60051faf9123bdb08571e723d80b546c8
Kinga <k.zawarty@intermarum.pl>
2021-06-08 13:02:53 +0200

    refactoring

--- project/src/Actors/Player.gd
+++ project/src/Actors/Player.gd
@@ -2,10 +2,10 @@ extends Actor
 
 
 func _physics_process(delta):
-	var is_jumping = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jumping = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction = calculate_direction()
-	velocity = calculate_velocity(velocity, direction, speed, is_jumping)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_velocity(_velocity, direction, speed, is_jumping)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 	
 func calculate_direction() -> Vector2: 
 		return Vector2(
@@ -15,15 +15,15 @@ func calculate_direction() -> Vector2:
 	);
 	
 func calculate_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, is_jumping: bool) -> Vector2:
-	var new_velocity = linear_velocity
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y += gravity * get_physics_process_delta_time() #delta = time elapsed til last frame => move constantly
+	var new = linear_velocity
+	new.x = speed.x * direction.x
+	new.y += gravity * get_physics_process_delta_time() #delta = time elapsed til last frame => move constantly
 	
 	if direction.y == -1.0: # jumping
-		new_velocity.y = speed.y * direction.y
+		new.y = speed.y * direction.y
 	if is_jumping:
-		new_velocity.y = 0.0
-	return new_velocity
+		new.y = 0.0
+	return new
 	
 
 
