496ef644628f33630f976aa6de44f7afd89ac7e5
Kinga <niu.ula.a@gmail.com>
2021-09-16 00:25:37 +0200

    intro & ending videos

--- project/src/Screens/EndScreen.gd
+++ project/src/Screens/EndScreen.gd
@@ -1,6 +1,20 @@
 extends Control
 
 onready var label: Label = get_node("Score")
+onready var audio_player: AudioStreamPlayer = $Audio
+onready var video_player: VideoPlayer = $VideoPlayer
 
 func _ready():
-	label.text = label.text % PlayerData.score
+	$Timer.connect("timeout", self, "turn_off_video")
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("skip"):
+		turn_off_video()
+		$Timer.stop()
+
+func turn_off_video():
+	video_player.visible = false
+	video_player.queue_free()
+	audio_player.play()
+	#label.text = "%s" % PlayerData.score  sth not working
---
+++ project/src/Screens/MainScreen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+onready var audio_player: AudioStreamPlayer = $Audio
+onready var video_player: VideoPlayer = $VideoPlayer
+
+func _ready():
+	$Timer.connect("timeout", self, "turn_off_video")
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("skip"):
+		turn_off_video()
+		$Timer.stop()
+
+func turn_off_video():
+	video_player.visible = false
+	video_player.queue_free()
+	audio_player.play()
